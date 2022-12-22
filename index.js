f24e2952ae67f18c8ca4d394b5f8f943414afcc5
Smith Ringtho <sringtho@gmail.com>
2022-12-22 09:22:02 +0300

    Added refresh button to reload

--- index.js
+++ index.js
@@ -8,21 +8,43 @@ document.addEventListener('click', function(e){
     if (e.target.dataset.action){
         handleLikeNopeBtnClick(e.target.dataset.action)
     }
+    if(e.target.id === "refresh"){
+        handleRefreshBtnClick()
+    }
 })
 
 function handleLikeNopeBtnClick(action){
     mainContentContainer.innerHTML += dog.getLikeNopeDogHtml(action)
+    console.log(action)
     setTimeout(()=> {
         const newDog = getNextDog()
+        action === 'like' ? newDog.hasBeenLiked = true : ""
+        
         render(newDog)
     }, 1500)
     
 }
 
+function handleRefreshBtnClick(){
+    document.location.reload()
+}
+
+function thankYou() {
+    return `
+    <div class="thanks">
+        <h2>Thank you for visiting TinDog!</h2>
+        <div class="btn-container like">
+        <img class="refresh-icon" id="refresh" src="/images/refresh.png" alt="refresh">
+        </div>
+    </div>
+    `
+}
+
 function getNextDog(){
     const nextDog = dogsData.shift()
     console.log(nextDog)
-    return nextDog ? new Dog(nextDog) : console.log("No more Dogs")
+    return nextDog ? new Dog(nextDog) : 
+    document.getElementById('main').innerHTML = thankYou()
 }
 
 function render(dog) {
