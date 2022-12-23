1f040b970bcd52dc78e8da6f2d41fbba4ec5c424
Smith Ringtho <sringtho@gmail.com>
2022-12-23 03:01:30 +0300

    Added more dog profiles

--- data.js
+++ data.js
@@ -22,6 +22,62 @@ const dogsData = [
         bio: "How you doin?",
         hasBeenSwiped: false,
         hasBeenLiked: false
+    },
+    {
+        name: "Richard",
+        avatar: "images/dog-richard.jpg",
+        age: 20,
+        bio: "Fool me once? Shame on you!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Ricardo",
+        avatar: "images/dog-ricardo.jpg",
+        age: 18,
+        bio: "Hey Dawg!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "David",
+        avatar: "images/dog-david.jpg",
+        age: 30,
+        bio: "I got to find that pur",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Manuel",
+        avatar: "images/dog-manuel.jpg",
+        age: 26,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Karsten",
+        avatar: "images/dog-karsten.jpg",
+        age: 25,
+        bio: "Where's my bone dawg?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Street",
+        avatar: "images/dog-street.jpg",
+        age: 25,
+        bio: "I was raised by the streets!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Winegeart",
+        avatar: "images/dog-karsten.jpg",
+        age: 25,
+        bio: "Bonjour mon amie?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
     }
 ]
 
